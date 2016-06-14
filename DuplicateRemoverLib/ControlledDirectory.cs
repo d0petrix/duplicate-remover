cf15c8d2f07e6543ef998fb86e80088e52f2dcfe
Timo Graw <tigraw@uos.de>
2016-06-14 23:50:10 +0200

    Added controlled configuration

---
+++ DuplicateRemoverLib/ControlledDirectory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuplicateRemoverLib
+{
+    public class ControlledDirectory
+    {
+        public string Name { get; private set; }
+
+        public string RootPath { get; private set; }
+
+        public string CacheFilename { get; private set; }
+
+        public ControlledDirectory RootNode;
+
+        public ControlledDirectory(string name, string rootPath)
+        {
+            RootPath = rootPath;
+            var appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            CacheFilename = Path.Combine(appDataPath, "Cache", Name + ".cache");
+        }
+
+        public void Load()
+        {
+
+        }
+    }
+}
