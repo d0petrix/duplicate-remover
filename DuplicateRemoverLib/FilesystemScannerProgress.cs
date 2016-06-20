d6280d379aa61c6f686ab8089b2feadf7aede805
Timo Graw <tigraw@uos.de>
2016-06-20 19:03:40 +0200

    Added Progress classes

---
+++ DuplicateRemoverLib/FilesystemScannerProgress.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuplicateRemoverLib
+{
+    public class FilesystemScannerProgress : IProgressManager
+    {
+
+        public int FilesCount;
+        public int FilesDone;
+        public int DirectoriesCount;
+        public int DirectoriesDone;
+
+        public double Value
+        {
+            get
+            {
+                return ((double)FilesCount + (double)DirectoriesCount) / ((double)FilesDone + (double) DirectoriesDone);
+            }
+        }
+    }
+}
---
+++ DuplicateRemoverLib/IProgressManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuplicateRemoverLib
+{
+    interface IProgressManager
+    {
+
+        double Value { get; }
+
+    }
+}
